3b32966f23243c9a34a285eb484dc4dfea47dc4c
Omar Gamal <agamyo168@gmail.com>
2024-12-13 20:05:09 +0200

    setting up logger

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv';
 import sequelize from './utils/db/connect';
 import notFoundMiddleware from './middlewares/notfound.middleware';
-import routes from './router/api/v1';
+import routes from './routes/api/v1';
+import { StatusCodes } from 'http-status-codes';
+import logger from './utils/db/logger';
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 //Routes
-
+//Health check
+app.get('/healthcheck', (req, res) => {
+  res.status(StatusCodes.ACCEPTED).json({
+    success: true,
+    msg: 'Response',
+  });
+});
 app.use('/api/v1/', routes);
 //Route Not Found redirction
 app.use(notFoundMiddleware);
@@ -22,11 +30,12 @@ app.use('/', (req, res) => {
 const start = async () => {
   try {
     await sequelize.sync();
+    logger.info('DB connected');
     app.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
+      logger.info(`App is running at http://localhost:${port}`);
     });
   } catch (err) {
-    console.error(err);
+    logger.error(err);
   }
 };
 start();
--- src/utils/db/logger.ts
+++ src/utils/db/logger.ts
@@ -1,2 +1,21 @@
-import logger from 'pino';
+import pino from 'pino';
+import pretty from 'pino-pretty';
 import dayjs from 'dayjs';
+
+const logger = pino({
+  level: 'info',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  },
+  base: {
+    pid: false,
+  },
+  timestamp: () => `,"time":"${dayjs().format()}`,
+});
+if (pretty.PinoPretty()) {
+  console.log('Yes');
+}
+export default logger;
